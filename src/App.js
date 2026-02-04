1bcfd8a9e54ebd948049aea734d3c2c868cf32c0
jinsungkim <kimswim02@gmail.com>
2026-02-04 21:54:48 +0900

    chore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Chat from "./Chat";
 import Home from "./page/Home";
+import Meet from "./page/Meet";
 import Archived from "./page/Archived";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element= {<Home />} />
+      <Route path="/" element= {<Meet />} />
       <Route path="/archived" element={<Archived />} />
     </Routes>
   );
