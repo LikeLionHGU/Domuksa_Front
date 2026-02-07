17bbe919d87a612868ecafd7b7e72f731afbd626
cm030627 <cm030627@gmail.com>
2026-02-07 23:25:21 +0900

    style : 프로필 팝업 디자인 수정

--- src/component/profile.js
+++ src/component/profile.js
@@ -1,22 +1,20 @@
 //홈페이지 프로필 사진 눌렀을 때 나오는 창
 
 import styles from "../CSS/Profile.module.css";
-import img from "../asset/testimg.png";
+import profileImg from "../asset/profile-test.png";
+import deleteIcon from "../asset/icon-delete.png"
 
 function Profile() {
   return (
     <div className={styles.body}>
       <div className={styles.main}>
+        <img className={styles.deleteIcon} src={deleteIcon} />
         <div className={styles.info}>
-          <img className={styles.modalImg} src={img} />
+          <img className={styles.modalImg} src={profileImg} />
           <div className={styles.text}>
             <div className={styles.name}>대표 김진성</div>
             <div className={styles.email}>email@email.com</div>
           </div>
-        </div>
-        <div className={styles.button}>
-          <div className={styles.setting}>setting</div>
-          <div className={styles.exit}>exit</div>
         </div>
       </div>
     </div>
