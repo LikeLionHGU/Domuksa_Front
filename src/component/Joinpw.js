8ba39acd411d33adf931c64beee4b00cd640c65d
cm030627 <cm030627@gmail.com>
2026-02-08 22:53:57 +0900

    feat : 참여코드 일치시 비밀번호 모달 활성화

--- src/component/Joinpw.js
+++ src/component/Joinpw.js
@@ -1,17 +1,24 @@
 //홈페이지 프로필 사진 눌렀을 때 나오는 창
 
-import styles from "../CSS/Profile.module.css";
+import styles from "../CSS/Joinpw.module.css";
 
 import deleteIcon from "../asset/icon-delete.png";
 import logoutIcon from "../asset/icon-logout.png";
 
-function Joinpw() {
-
+function Joinpw({ onChange }) {
 
   return (
-    <div className={styles.extradiv}>
-      <div className={styles.maindiv}>
-        
+    <div>
+      <div className={styles.extradiv}>
+        <div className={styles.maindiv}>
+          <div className={styles.modal}>
+            <img
+              className={styles.deleteIcon}
+              src={deleteIcon}
+              onClick={() => onChange(false)}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
