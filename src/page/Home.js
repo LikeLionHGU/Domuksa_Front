a2a1e7041f29d801fbf66a4270477153782edc9e
cm030627 <cm030627@gmail.com>
2026-02-09 17:41:56 +0900

    merge : 머지준비

--- src/page/Home.js
+++ src/page/Home.js
@@ -2,27 +2,47 @@ import { useEffect, useState } from "react";
 import styles from "../CSS/Home.module.css";
 import logoImg from "../asset/icon-logo.png";
 import completeImg from "../asset/icon-complete.png";
-import addImg from "../asset/icon-add_white.png"
+import addImg from "../asset/icon-add_white.png";
 import roomImg from "../asset/icon-meetingroom.png";
 import Profile from "../component/Profile";
+import Joinpw from "../component/Joinpw";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
 
-  const [open, setOpen] = useState(false);
+  const [profileOpen, setProfileOpen] = useState(false);
+  const [pwOpen, setPwOpen] = useState(false);
 
   const [picture, setPicture] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
+  const [value, setValue] = useState("");
+  const [code, setCode] = useState("");
+
+  const testCode = "1234567890";
+
   const [room, setRoom] = useState([]);
 
-  function isOpen() {
-    if (open == false) {
-      setOpen(true);
-    } else if (open == true) {
-      setOpen(false);
+  const activeEnter = (e) => {
+    if (e.key === "Enter") {
+      setCode(value);
+    }
+  };
+
+  function isRightPw() {
+    if (testCode === code) {
+      setPwOpen(true);
+      setCode("");
+    }
+  }
+
+  function isProfileOpen() {
+    if (profileOpen == false) {
+      setProfileOpen(true);
+    } else if (profileOpen == true) {
+      setProfileOpen(false);
     }
   }
 
@@ -39,6 +59,7 @@ function Home() {
   }
 
   useEffect(() => {
+    isRightPw();
     const token = localStorage.getItem("accessToken");
     if (!token) {
       return;
@@ -50,12 +71,15 @@ function Home() {
       setEmail(user.email);
       setPicture(user.profileUrl);
     }
-  }, []);
+  }, [code]);
+
+  console.log(code);
 
   return (
     <div>
       <div className={styles.extradiv}>
         <div className={styles.Maindiv}>
+          {pwOpen === true ? <Joinpw onChange={setPwOpen} code={code} /> : null}
           <div className={styles.header}>
             <img className={styles.logo} src={logoImg} onClick={goLanding} />
 
@@ -64,11 +88,11 @@ function Home() {
               <img
                 className={styles.prifileImg}
                 src={picture}
-                onClick={isOpen}
+                onClick={isProfileOpen}
               />
-              {open === true ? (
+              {profileOpen === true ? (
                 <Profile
-                  onChange={setOpen}
+                  onChange={setProfileOpen}
                   user={{ name: name, email: email, picture: picture }}
                 />
               ) : null}
@@ -77,12 +101,16 @@ function Home() {
 
           <div className={styles.menu}>
             <div className={styles.new} onClick={goNew}>
-              <img className={styles.addBtn} src={addImg}/>
+              <img className={styles.addBtn} src={addImg} />
             </div>
 
             <div className={styles.join}>
               회의 참여를 위해 코드를 입력해 주세요
-              <input></input>
+              <input
+                className={styles.code}
+                onChange={(e) => setValue(e.target.value)}
+                onKeyDown={(e) => activeEnter(e)}
+              ></input>
             </div>
           </div>
 
@@ -95,42 +123,6 @@ function Home() {
             <div className={styles.archive} onClick={goArchived}>
               <img className={styles.completeImg} src={completeImg} />
               <div className={styles.complete}>완료됨</div>
-            </div>
-            <div className={styles.room}>
-              <img className={styles.roomImg} src={roomImg} />
-              <div className={styles.roomName}>2025 두먹사 회의</div>
-            </div>
-            <div className={styles.room}>
-              <img className={styles.roomImg} src={roomImg} />
-              <div className={styles.roomName}>2025 두먹사 회의</div>
-            </div>
-            <div className={styles.room}>
-              <img className={styles.roomImg} src={roomImg} />
-              <div className={styles.roomName}>2025 두먹사 회의</div>
-            </div>
-            <div className={styles.room}>
-              <img className={styles.roomImg} src={roomImg} />
-              <div className={styles.roomName}>2025 두먹사 회의</div>
-            </div>
-            <div className={styles.room}>
-              <img className={styles.roomImg} src={roomImg} />
-              <div className={styles.roomName}>2025 두먹사 회의</div>
-            </div>
-            <div className={styles.room}>
-              <img className={styles.roomImg} src={roomImg} />
-              <div className={styles.roomName}>2025 두먹사 회의</div>
-            </div>
-            <div className={styles.room}>
-              <img className={styles.roomImg} src={roomImg} />
-              <div className={styles.roomName}>2025 두먹사 회의</div>
-            </div>
-            <div className={styles.room}>
-              <img className={styles.roomImg} src={roomImg} />
-              <div className={styles.roomName}>2025 두먹사 회의</div>
-            </div>
-            <div className={styles.room}>
-              <img className={styles.roomImg} src={roomImg} />
-              <div className={styles.roomName}>2025 두먹사 회의</div>
             </div>
             <div className={styles.room}>
               <img className={styles.roomImg} src={roomImg} />
