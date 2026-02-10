8bdb7f72ca8c25b55d4511fa80bfadd3bd9537c5
cm030627 <cm030627@gmail.com>
2026-02-10 14:05:21 +0900

    fix : 프로필 정보 받아오기 수정

--- src/page/Landing.js
+++ src/page/Landing.js
@@ -5,33 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 function Landing() {
   const navigate = useNavigate();
-  const [logined, setLogined] = useState(false);
-  const [name, setName] = useState(null);
-  const [picture, setPicture] = useState(null);
 
-  useEffect(() => {
-    const token = localStorage.getItem("accessToken");
-    if (!token) {
-      setLogined(false);
-      return;
-    }
-    setLogined(true);
-    if (token) {
-      const User = JSON.parse(localStorage.getItem("userInfo"));
-      setName(User.name);
-      setPicture(User.picture);
-    }
-
-    axios
-      .get(`${process.env.REACT_APP_HOST_URL}/mypage`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .catch((error) => {
-        console.error("마이페이지 정보 가져오기 실패:", error);
-      });
-  }, []);
 
   function goHome (){
     navigate("/home");
