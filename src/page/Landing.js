90f85480187ca08d9220cf85a8e312a65d6f2a93
cm030627 <cm030627@gmail.com>
2026-02-10 14:18:45 +0900

    delete : 코드 정리

--- src/page/Landing.js
+++ src/page/Landing.js
@@ -1,19 +1,16 @@
-import { useEffect, useRef, useState } from "react";
 import GoogleLogin from "../component/loginPage";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 function Landing() {
   const navigate = useNavigate();
 
-
-  function goHome (){
+  function goHome() {
     navigate("/home");
   }
 
   return (
     <>
-    <div onClick={goHome}>홈으로 가기 임시 버튼</div>
+      <div onClick={goHome}>홈으로 가기 임시 버튼</div>
       <GoogleLogin />
     </>
   );
