1b7f73e0d595afacb08f8960c9972345f2eaec68
cm030627 <cm030627@gmail.com>
2026-02-18 23:46:58 +0900

    style : 랜딩페이지 아이콘

--- src/page/Landing.js
+++ src/page/Landing.js
@@ -1,6 +1,12 @@
 import GoogleLogin from "../component/loginPage";
 import styles from "../CSS/Landing.module.css";
-import logoImg from "../asset/icon-logo.png"
+import logoImg from "../asset/icon-logo.png";
+import landingBg from "../asset/background_line-landing.png";
+import file from "../asset/Mainicon-color-file.png";
+import comment from "../asset/Mainicon-color-comment.png";
+import vote from "../asset/Mainicon-color-vote.png";
+import ai from "../asset/Mainicon-color-Ai.png";
+import people from "../asset/icon-meetingroom.png";
 
 function Landing() {
   localStorage.removeItem("accessToken");
@@ -13,12 +19,28 @@ function Landing() {
       <div className={styles.extradiv}>
         <div className={styles.Maindiv}>
           <div className={styles.menu}>
-            <img className={styles.logoImg} src={logoImg} />
+            <img className={styles.logoImg} src={logoImg} alt="이음 로고" />
             <div className={styles.dms}>두먹사</div>
             <div className={styles.emmm}>이음</div>
             <div className={styles.service}>서비스</div>
             <div className={styles.ask}>문의</div>
             <GoogleLogin />
+          </div>
+
+          <div className={styles.first}>
+            <img className={styles.line} src={landingBg} alt="라인" />
+            <div className={styles.peoplemom}>
+              <img className={styles.people} src={people} alt="사람들" />
+            </div>
+
+            <div className={styles.icon}>
+              <div className={styles.progress}>• 진행중</div>
+              <img className={styles.file} src={file} alt="파일" />
+              <img className={styles.comment} src={comment} alt="코멘트" />
+              <img className={styles.vote} src={vote} alt="투표" />
+              <img className={styles.ai} src={ai} alt="ai" />
+              <div className={styles.complete}>• 완료</div>
+            </div>
           </div>
         </div>
       </div>
