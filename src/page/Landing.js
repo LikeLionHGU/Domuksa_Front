d2c67cc44e20ce4a36f9b0bf2d6ce2dfcbd2f641
cm030627 <cm030627@gmail.com>
2026-02-19 19:15:52 +0900

    style : 랜딩페이지 3/4 완성

--- src/page/Landing.js
+++ src/page/Landing.js
@@ -13,6 +13,9 @@ import dm from "../asset/icon-dm-landing.png";
 import fimg1 from "../asset/landing-function1.png";
 import fimg2 from "../asset/landing-function2.png";
 import fimg3 from "../asset/landing-function3.png";
+import simg1 from "../asset/landing-strength1.png";
+import simg2 from "../asset/landing-strength2.png";
+import simg3 from "../asset/landing-strength3.png";
 
 function Landing() {
   localStorage.removeItem("accessToken");
@@ -75,6 +78,7 @@ function Landing() {
               </div>
             </div>
           </div>
+
           <div className={styles.firstLine}>
             <div>
               “혹시 오늘도 ‘음...’ 하다가 <strong>회의 </strong>
@@ -85,6 +89,7 @@ function Landing() {
               당사자들이 직접 <strong>마침표</strong>를 찍습니다
             </div>
           </div>
+
           <div className={styles.second}>
             <div className={styles.secondText}>
               <div className={styles.secondText1}>
@@ -98,7 +103,7 @@ function Landing() {
               <div className={styles.function}>
                 <img className={styles.functionImg} src={fimg1} alt="기능1" />
                 <div className={styles.functionTitle}>익명 기반 소통</div>
-                <div className={styles.functionDetail}>
+                <div className={styles.functionDetail1}>
                   모든 툴 익명 보장. 목소리 큰 팀원이 이끄는 회의는 끝났습니다.
                   당신의 ‘진짜’ 생각을 편하게 던지세요
                 </div>
@@ -106,20 +111,78 @@ function Landing() {
               <div className={styles.function}>
                 <img className={styles.functionImg} src={fimg2} alt="기능2" />
                 <div className={styles.functionTitle}>안건보드</div>
-                <div className={styles.functionDetail}>
+                <div className={styles.functionDetail2}>
                   회의 주제가 한눈에 안건보드가 흐름을 잡아줘요
                 </div>
               </div>
               <div className={styles.function}>
                 <img className={styles.functionImg} src={fimg3} alt="기능3" />
                 <div className={styles.functionTitle}>회의 툴 블록</div>
-                <div className={styles.functionDetail}>
-                  의견 제시에서 결론까지 블록 하나로<br/> 더 이상 툴 사이에서 길을
-                  잃지 마세요
+                <div className={styles.functionDetail3}>
+                  의견 제시에서 결론까지 블록 하나로
+                  <br /> 더 이상 툴 사이에서 길을 잃지 마세요
                 </div>
               </div>
             </div>
           </div>
+
+          <div className={styles.secondLine}>
+            <div>
+              준비부터 결과까지, 단 <strong>세 단계면 </strong> 충분합니다
+            </div>
+            <div>
+              “<strong>익명성</strong>으로 입을 틔우고,
+              <strong> 안건 보드</strong>로 방향을 잡고,
+              <strong> 회의 툴 블록</strong>으로 결론을 맺습니다.”
+            </div>
+          </div>
+
+          <div className={styles.third}>
+            <div className={styles.thirdText}>
+              <div className={styles.thirdText1}>익명이 중요한 이유</div>
+              <div className={styles.thirdText2}>
+                침묵을 깨는 익명 블록으로 의견 발산부터 AI 요약까지 끊김 없이
+                <span> 이음</span>
+              </div>
+            </div>
+            <div className={styles.functions}>
+              <div className={styles.function}>
+                <img className={styles.functionImg} src={simg1} alt="강점1" />
+                <div className={styles.strength1}>
+                  <strong>공개적인 회의 환경</strong>은 발언을 위축시킨다
+                </div>
+              </div>
+              <div className={styles.function}>
+                <img className={styles.functionImg} src={simg2} alt="강점2" />
+                <div className={styles.strength2}>
+                  <strong>심리적 안전</strong>과<strong> 의견 제시 방식</strong>
+                  은 회의 참여도에 직접적인 영향을 준다
+                </div>
+              </div>
+              <div className={styles.function}>
+                <img className={styles.functionImg} src={simg3} alt="강점3" />
+                <div className={styles.strength3}>
+                  회의가 <strong>비효율적</strong>일수록
+                  <strong> 발언과 참여가 감소</strong>한다
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.thirdLine}>
+            <div>
+              <strong>이음</strong>은 모든 의견을 살립니다
+            </div>
+            <div>
+              <strong>87%</strong>의 대학생이 선택한 익명 소통 방식. 비난받을
+              걱정 없이 당신의 가장 날카로운 생각을 던지세요
+            </div>
+            <div className={styles.start} onClick={handleGoogleLogin}>
+              시작하기
+            </div>
+          </div>
+
+          <div className={styles.footer}></div>
         </div>
       </div>
     </>
