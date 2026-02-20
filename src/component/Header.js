3648f9ef107f0d1fe2928b5e7eb552e64cee7caf
cm030627 <cm030627@gmail.com>
2026-02-20 23:31:20 +0900

    style : 프로필 모달 디자인 수정

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-import style from "../CSS/Header.module.css"
+import style from "../CSS/Header.module.css";
 
 import copy from "../asset/icon-copy.png";
 import logo from "../asset/icon-logo.png";
@@ -10,16 +10,23 @@ import out from "../asset/icon-out.png";
 
 //사이트 메인 아이콘 + 구글 아이콘 + 방번호 + 완료/진행중 상태
 function Header({
-  setLogoutmodal, setEmail, setName, setPicture, setState,
-  token, isHost, code, state, roomId, name, email, picture
+  setLogoutmodal,
+  setEmail,
+  setName,
+  setPicture,
+  setState,
+  token,
+  isHost,
+  code,
+  state,
+  roomId,
+  name,
+  email,
+  picture,
 }) {
-
-
-
   const navigate = useNavigate();
   const [profileOpen, setProfileOpen] = useState(false);
   const ModalRef = useRef(null);
-
 
   useEffect(() => {
     if (state === "running") {
@@ -30,7 +37,6 @@ function Header({
   }, [state]);
 
   useEffect(() => {
-
     function HandClickoutsideofModal(e) {
       if (ModalRef.current && !ModalRef.current.contains(e.target)) {
         setProfileOpen(false);
@@ -39,7 +45,8 @@ function Header({
 
     document.addEventListener("mousedown", HandClickoutsideofModal);
 
-    return () => document.removeEventListener("mousedown", HandClickoutsideofModal);
+    return () =>
+      document.removeEventListener("mousedown", HandClickoutsideofModal);
   }, [ModalRef]);
 
   useEffect(() => {
@@ -52,34 +59,35 @@ function Header({
     setName(JSON.parse(user).name);
     setEmail(JSON.parse(user).email);
     setPicture(JSON.parse(user).profileUrl);
-
   }, []);
 
   function handleButton() {
     if (state === "running") {
       axios
-        .patch(`
+        .patch(
+          `
           ${process.env.REACT_APP_HOST_URL}/room/${roomId}/state`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
             },
-          })
+          },
+        )
         .catch((error) => {
           console.error("마이페이지 정보 가져오기 실패:", error);
         });
       return;
     } else {
-
-
       axios
-        .patch(`
+        .patch(
+          `
           ${process.env.REACT_APP_HOST_URL}/room/${roomId}/state`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
             },
-          })
+          },
+        )
         .catch((error) => {
           console.error("마이페이지 정보 가져오기 실패:", error);
         });
@@ -87,7 +95,9 @@ function Header({
   }
 
   function handleRoomNumber() {
-    navigator.clipboard.writeText(document.getElementById("RoomNumber").innerText);
+    navigator.clipboard.writeText(
+      document.getElementById("RoomNumber").innerText,
+    );
     alert("클립이 복사되었습니다!");
   }
 
@@ -95,45 +105,62 @@ function Header({
     navigate("/home");
   }
 
-
   return (
     <div className={style.Maindiv}>
-
       <div className={style.Left}>
         <img src={logo} alt="logo" onClick={() => backtohome()} />
       </div>
 
       <div className={style.Right}>
-        <div id="state" className={state === "running" ? style.StateIng : style.StateFinish} onClick={() => isHost && handleButton()}><strong>•</strong>&nbsp;{state === "running" ? "진행중" : "완료"}</div>
-        <div id="RoomNumber" className={style.Number} onClick={() => handleRoomNumber()}>{code}<img alt="copy" src={copy} /></div>
-        <img alt="profileicon" src={picture} onClick={() => setProfileOpen(true)} />
+        <div
+          id="state"
+          className={state === "running" ? style.StateIng : style.StateFinish}
+          onClick={() => isHost && handleButton()}
+        >
+          <strong>•</strong>&nbsp;{state === "running" ? "진행중" : "완료"}
+        </div>
+        <div
+          id="RoomNumber"
+          className={style.Number}
+          onClick={() => handleRoomNumber()}
+        >
+          {code}
+          <img alt="copy" src={copy} />
+        </div>
+        <img
+          alt="profileicon"
+          src={picture}
+          onClick={() => setProfileOpen(true)}
+        />
 
         {/*--> 모달 <--*/}
-        {profileOpen ? <div className={style.ProfileModal} ref={ModalRef}>
-          <div className={style.Maindiv}>
-            <div className={style.Subdiv}>
-              <img src={picture} alt="googleicon" />
-              <div className={style.ModalInfo}>
-                <div className={style.Name}>{name}</div>
-                <span>{email}</span>
+        {profileOpen ? (
+          <div className={style.ProfileModal} ref={ModalRef}>
+            <div className={style.Maindiv}>
+              <div className={style.Subdiv}>
+                <img src={picture} alt="googleicon" />
+                <div className={style.ModalInfo}>
+                  <div className={style.Name}>{name}</div>
+                  <span>{email}</span>
+                </div>
               </div>
             </div>
+            <button
+              onClick={() => {
+                setLogoutmodal(true);
+                setProfileOpen(false);
+              }}
+            >
+              <img alt="out" src={out} />
+              로그아웃
+            </button>
           </div>
-          <button
-            onClick={() => {
-              setLogoutmodal(true);
-              setProfileOpen(false);
-            }}>
-            <img alt="out"
-              src={out}
-            />Logout</button>
-        </div> : <></>}
+        ) : (
+          <></>
+        )}
       </div>
-
     </div>
   );
 }
 
 export default Header;
-
-
