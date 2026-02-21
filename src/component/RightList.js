d7a6f17cd4c4a84bf6114546473ab9e1f85ea0aa
Jinsung941 <kimswim02@gmail.com>
2026-02-21 19:23:44 +0900

    Update RightList.js

--- src/component/RightList.js
+++ src/component/RightList.js
@@ -27,34 +27,25 @@ function RightList({ now, token, isHost, roomId, clickedAgendaId, socketFile, so
   const [AIState, setAIState] = useState(false);
 
   useEffect(() => {
+
     if (clickedAgendaId === null || token === null) {
       return;
     }
     axios
-      .get(`${process.env.REACT_APP_HOST_URL}/agenda/${clickedAgendaId}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
+      .get(
+        `${process.env.REACT_APP_HOST_URL}/agenda/${clickedAgendaId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
       .then((res) => {
         console.log(res.data.config);
         //[config===기능State]
-        if (res.data.config.voteEnabled === true) setVoteState(true);
-        else {
-          setVoteState(false);
-        }
-        if (res.data.config.commentEnabled === true) setChatState(true);
-        else {
-          setChatState(false);
-        }
-        if (res.data.config.fileEnabled === true) setFileState(true);
-        else {
-          setFileState(false);
-        }
-        if (res.data.config.aiSummaryEnabled === true) setAIState(true);
-        else {
-          setAIState(false);
-        }
+        if (res.data.config.voteEnabled === true) setVoteState(true); else { setVoteState(false) }
+        if (res.data.config.commentEnabled === true) setChatState(true); else { setChatState(false) }
+        if (res.data.config.fileEnabled === true) setFileState(true); else { setFileState(false) }
+        if (res.data.config.aiSummaryEnabled === true) setAIState(true); else { setAIState(false) }
       })
       .catch((error) => {
         if (error.status === 500) {
@@ -64,102 +55,83 @@ function RightList({ now, token, isHost, roomId, clickedAgendaId, socketFile, so
   }, [clickedAgendaId, socketFile, socketComment, socketVote, socketAI]);
 
   const Mode = {
-    basic: (
-      <>
-        <div className={style.Maindiv}>
-          <div className={style.Subdiv}>
-            <div
-              className={style.Block}
-              id="file"
-              onClick={() => {
-                if (clickedAgendaId === null) {
-                  alert("안건을 선택해주세요!");
-                  return;
-                }
-                setMode("File");
-              }}
-              onMouseEnter={() => setHover("File")}
-              onMouseLeave={() => setHover("")}
-            >
-              <img
-                alt="Mainicon-file"
-                className={style.imgFile}
-                src={FileState || hover === "File" ? iconFileColor : iconFile}
-              />
-              <h3>파일</h3>
-            </div>
-            <div
-              className={style.Block}
-              id="comment"
-              onClick={() => {
-                if (clickedAgendaId === null) {
-                  alert("안건을 선택해주세요!");
-                  return;
-                }
-                setMode("Comment");
-              }}
-              onMouseEnter={() => setHover("Comment")}
-              onMouseLeave={() => setHover("")}
-            >
-              <img
-                alt="Mainicon-comment"
-                className={style.imgComment}
-                src={
-                  ChatState || hover === "Comment" ? iconChatColor : iconChat
-                }
-              />
-              <h3>코멘트</h3>
-            </div>
-            <div
-              className={style.Block}
-              id="vote"
-              onClick={() => {
-                if (clickedAgendaId === null) {
-                  alert("안건을 선택해주세요!");
-                  return;
-                }
-                setMode("Vote");
-              }}
-              onMouseEnter={() => setHover("Vote")}
-              onMouseLeave={() => setHover("")}
-            >
-              <img
-                alt="Mainicon-vote"
-                className={style.imgVote}
-                src={VoteState || hover === "Vote" ? iconVoteColor : iconVote}
-              />
-              <h3>투표</h3>
-            </div>
+    basic: <>
+      <div className={style.Maindiv}>
+        <div className={style.Subdiv}>
+          <div
+            className={style.Block}
+            id="file"
+            onClick={() => {
+              if (clickedAgendaId === null) {
+                alert("안건을 선택해주세요!");
+                return;
+              }
+              setMode("File");
+            }}
+            onMouseEnter={() => setHover("File")}
+            onMouseLeave={() => setHover("")}
+          >
+            <img alt="Mainicon-file" className={style.imgFile} src={FileState || hover === "File" ? iconFileColor : iconFile} />
+            <h3>파일</h3>
           </div>
-          <div className={style.Subdiv}>
-            <div
-              className={style.BigBlock}
-              id="ai"
-              onClick={() => {
-                if (clickedAgendaId === null) {
-                  alert("안건을 선택해주세요!");
-                  return;
-                }
-                setMode("AI");
-              }}
-              onMouseEnter={() => setHover("AI")}
-              onMouseLeave={() => setHover("")}
-            >
-              <img
-                alt="Mainicon-ai"
-                className={style.imgAI}
-                src={AIState || hover === "AI" ? iconAIColor : iconAI}
-              />
-              <h3>AI 요약</h3>
-            </div>
-            <div className={style.SmallBlock} id="users">
-              <Users
-                socketVoteId={socketVoteId}
-                socketUser={socketUser}
-                token={token}
-                roomId={roomId}
-              />
-            </div>
+          <div
+            className={style.Block}
+            id="comment"
+            onClick={() => {
+              if (clickedAgendaId === null) {
+                alert("안건을 선택해주세요!");
+                return;
+              }
+              setMode("Comment");
+            }}
+            onMouseEnter={() => setHover("Comment")}
+            onMouseLeave={() => setHover("")}>
+            <img alt="Mainicon-comment" className={style.imgComment} src={ChatState || hover === "Comment" ? iconChatColor : iconChat} />
+            <h3>코멘트</h3>
+          </div>
+          <div
+            className={style.Block}
+            id="vote"
+            onClick={() => {
+              if (clickedAgendaId === null) {
+                alert("안건을 선택해주세요!");
+                return;
+              }
+              setMode("Vote");
+            }}
+            onMouseEnter={() => setHover("Vote")}
+            onMouseLeave={() => setHover("")}>
+            <img alt="Mainicon-vote" className={style.imgVote} src={VoteState || hover === "Vote" ? iconVoteColor : iconVote} />
+            <h3>투표</h3>
+          </div>
+        </div>
+        <div
+          className={style.Subdiv}>
+          <div
+            className={style.BigBlock}
+            id="ai"
+            onClick={() => {
+              if (clickedAgendaId === null) {
+                alert("안건을 선택해주세요!");
+                return;
+              }
+              setMode("AI");
+            }}
+            onMouseEnter={() => setHover("AI")}
+            onMouseLeave={() => setHover("")}>
+            <img alt="Mainicon-ai" className={style.imgAI} src={AIState || hover === "AI" ? iconAIColor : iconAI} />
+            <h3>AI 요약</h3>
+          </div>
+          <div
+            className={style.SmallBlock}
+            id="users"
+          >
+            <Users
+              socketVoteId={socketVoteId}
+              socketUser={socketUser}
+              token={token}
+              roomId={roomId}
+            />
           </div>
         </div>
       </div>
